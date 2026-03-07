919761a33847215d00bae642a3369d0aeb719bf1
Divya Gawade <divyagawade15@gmail.com>
2026-03-07 11:37:42 +0530

    reversestring

---
+++ ReverseString.java
@@ -0,0 +1,25 @@
+//how to reverse a string in java  "india"
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String s = "india";           //starting indix 0 to 4 
+
+        int len =s.length();          //length of string is 5
+
+        String rev = "";               //empty string
+
+        for(int i=len-1; i>=0; i--)
+        {
+            rev=rev+s.charAt(i) ;           //logic 0+a =a 
+                                            //a+i =i
+                                            //i+d =d
+                                            //d+n =n
+                                            //n+i =i
+
+        }
+
+        System.out.println("Reversed string: " + rev);        //output: Reversed string: aidni
+
+    }
+}
